419229f2cc09536fb4ba98c7b6b28faf669abfb5
Jae Yong Kim <kim900180@gmail.com>
2018-08-02 20:02:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,8 @@
 class Song
   attr_accessor :name, :artist, :genre
 
-  def initialize(song, artist, genre)
-    @song = song
+  def initialize(name, artist, genre)
+    @song = name
     @artist = artist
     @genre = genre
   end
